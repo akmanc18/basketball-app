8f154d039cd52fe775fdba5de69fa5b6402b38e3
yesod30 <samu.9301295@live.it>
2023-11-03 12:12:10 +0100

    implemented the stats page

--- src/util/entities.ts
+++ src/util/entities.ts
@@ -4,11 +4,57 @@ export interface Position
     yPos: number;
 }
 
+export interface Team
+{
+    team_id: number,
+    team_name: string
+}
+
 export interface Player
 {
     player_id: number;
     player_name: string;
     team_id: number | null;
 }
+
+export interface Game
+{
+    game_date: string,
+    game_id: number,
+    score_team_1: number | null,
+    score_team_2: number | null,
+    team_1: number,
+    team_2: number
+}
+
+export interface Shot
+{
+    game_id: number,
+    game_timer: string,
+    player_id: number,
+    point_value: number,
+    shot_id: number,
+    successful: boolean
+}
+
+export interface PlayerTableData
+{
+    playerId: number,
+    playerName: string;
+    playerNumber: number;
+    threePointShot: ShotData;
+    twoPointShot: ShotData;
+    onePointShot: ShotData;
+    blocks: number;
+    turnovers: number
+}
+
+export interface ShotData
+{
+    attempted: number;
+    successful: number
+    percentage: number;
+}
+
 
 export type ShotResult = "hit" | "missed" | "blocked";
